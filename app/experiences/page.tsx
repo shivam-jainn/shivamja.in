46223a2ae686753a42dfb39f8d5f69e431ed406d
shivam-jainn <shivam.clgstash@gmail.com>
2024-10-17 12:12:09 -0400

    added experiences

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import ExpCard from '@/components/experiencecard/ExpCard'
-export default function page() {
+import React from 'react';
+import ExpCard from '@/components/experiencecard/ExpCard';
+import experiences from './experience.json';
+
+export default function Page() {
   return (
     <div className='h-full w-full p-8 overflow-auto'>
-      <h1 className='text-5xl font-bold '>Experiences ✨</h1>
-
-      <ExpCard startDate='26/04/24' endDate='31/09/2024' jobTitle='Fullstack developer' companyName='Swiftride' techStack={["react"]} responsibilities='some'/>
-    
+      <h1 className='text-5xl font-bold'>Experiences ✨</h1>
+      {experiences.map((experience, index) => (
+        <ExpCard
+          key={index}
+          startDate={experience.startDate}
+          endDate={experience.endDate}
+          jobTitle={experience.jobTitle}
+          companyName={experience.companyName}
+          techStack={experience.techStack}
+          responsibilities={experience.responsibilities.map((resp, idx) => (
+            <li key={idx}>{resp}</li>
+          ))}
+          link={experience.link} 
+        />
+      ))}
     </div>
-  )
+  );
 }
